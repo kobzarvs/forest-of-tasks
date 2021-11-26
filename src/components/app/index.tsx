0ffc636db339fb9881e60bd28e7b1de5601184f6
kobzarvs <kobzarvs@gmail.com>
2021-11-26 04:15:28 +0300

    initial

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,48 +1,258 @@
-import React from 'react';
-import {Clock} from '../../features/clock/components';
+import React, {ReactChild, ReactNode, useCallback, useEffect, useState} from 'react';
 import './styles.css';
 
 
-export const App = () => {
-  return (
-    <div className="flex flex-col place-items-center">
-      <h1 className="font-bold text text-gray-700 mt-4">Example clock component</h1>
-      <Clock />
+type TaskFields = {
+  name?: string;
+  description?: string;
+  completed?: boolean;
+}
 
-      <div
-        className="grid xs:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xs:w-2/3 gap-2 place-items-center place-content-center p-2 ">
-        <div className="text-2xl font-bold pt-2 pb-1 text-center md:col-span-2 xl:col-span-3 text-gray-700">
-          Template includes
-        </div>
-        <a href="https://parceljs.org/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/parcel.png" alt="parcel-logo" className="logo" />
-        </a>
-        <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/typescript.png" alt="typescript logo" className="logo" />
-        </a>
-        <a href="https://ru.reactjs.org/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/react.png" alt="react logo" className="logo" />
-        </a>
-        <a href="https://effector.dev/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/effector.png" alt="effector logo" className="logo px-5" />
-        </a>
-        <a href="https://github.com/effector/patronum" target="_blank" rel="noopener noreferrer">
-          <img src="/images/patronum.png" alt="patronum logo" className="logo px-4" />
-        </a>
-        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/tailwindcss.png" alt="tailwindcss logo" className="tailwind-logo" />
-        </a>
-        <a href="https://jestjs.io/ru/" target="_blank" rel="noopener noreferrer" className="logo text-white text-2xl"
-           style={{width: 220, background: '#15c213'}}>
-          JEST
-        </a>
-        <a href="https://testing-library.com/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/testing-library.png" alt="testing-library logo" className="logo" />
-        </a>
-        <a href="https://eslint.org/" target="_blank" rel="noopener noreferrer">
-          <img src="/images/eslint.png" alt="eslint logo" className="logo" />
-        </a>
+
+interface TodoItem {
+  id: string;
+  name: string;
+  description: string;
+  completed: boolean;
+  parent?: string | undefined;
+}
+
+
+type ITodoListIndex = { [key: string]: TodoItem };
+
+
+const LinkToTask = ({className, task}: { className?: string; task: TodoItem }) => (
+  <a className={`link-to-parent ${className}`}
+     href={`#${task.id}`}
+  >
+    ↑ {task.name}
+  </a>
+);
+
+const Task = ({tasks, task, deleteTask, addTask, updateTask}: {
+  tasks: ITodoListIndex;
+  task: TodoItem;
+  children?: React.ReactChildren | React.ReactNode;
+  deleteTask: (task: TodoItem) => void;
+  updateTask: (task: TodoItem, values: TaskFields) => void;
+  addTask: (parent: string | undefined) => void;
+}) => (
+  <details className="task" id={task.id.toString()}>
+    <summary>
+      {task.name ? <strong>{task.name}</strong> : '<Noname Task>'}
+      {task.parent ? <LinkToTask className="right" task={tasks[task.parent]} /> : null}
+    </summary>
+    <div className="task-form">
+      <label className="task-name-input">
+        Name:
+        <input type="text"
+               value={task.name}
+               onChange={e => updateTask(task, {name: e.target.value})}
+        />
+      </label>
+      <label className="task-description-input">
+        Description:
+        <textarea rows={3}
+                  value={task.description}
+                  onChange={e => updateTask(task, {description: e.target.value})}
+        />
+      </label>
+      <label className="task-completed-input">
+        <input type="checkbox"
+               checked={task.completed}
+               onChange={e => updateTask(task, {completed: e.target.checked})}
+        />
+        is completed
+      </label>
+      <button className="btn secondary" onClick={() => deleteTask(task)}>
+        Delete task
+      </button>
+    </div>
+    <TodoList tasks={tasks}
+              root={task}
+              deleteTask={deleteTask}
+              addTask={addTask}
+              updateTask={updateTask}
+    />
+  </details>
+);
+
+
+const TodoList = ({tasks, root, deleteTask, addTask, updateTask}: {
+  tasks: ITodoListIndex;
+  root?: TodoItem;
+  deleteTask: (task: TodoItem) => void;
+  updateTask: (task: TodoItem, values: TaskFields) => void;
+  addTask: (parent: string | undefined) => void;
+}) => {
+  if (!root) {
+    root = undefined;
+  }
+
+  const taskList = Object.values(tasks).filter(task => task.parent === root?.id);
+
+  return (
+    <div className="task-list">
+      {root && taskList.length !== 0 && <strong style={{marginBottom: '-1em'}}>Sub Task List</strong>}
+      {taskList.map(task => (
+        <Task key={task.id}
+              tasks={tasks}
+              task={task}
+              deleteTask={deleteTask}
+              addTask={addTask}
+              updateTask={updateTask}
+        />
+      ))}
+      <div className="task-actions">
+        <button className="btn primary" onClick={() => addTask(root?.id)}>Add Task</button>
+        {root?.name && <LinkToTask task={root} />}
       </div>
+    </div>
+  );
+};
+
+
+const nextId = () => (Math.random() * 100000 + Date.now() % 100000).toString(36);
+
+const createTask = (parent: string | undefined) => ({
+  id: nextId(),
+  name: '',
+  description: '',
+  completed: false,
+  parent,
+});
+
+const LOCAL_STORAGE_ID = 'forest-tasks';
+
+const save = (tasks: ITodoListIndex) => {
+  try {
+    localStorage.setItem(LOCAL_STORAGE_ID, JSON.stringify(tasks));
+    console.log(JSON.stringify(tasks));
+    return true;
+  } catch (e) {
+    console.error(e);
+  }
+  return false;
+};
+
+const load = (): ITodoListIndex => {
+  try {
+    const persistData = localStorage.getItem(LOCAL_STORAGE_ID);
+    console.log('ls', persistData);
+    if (persistData) {
+      return JSON.parse(persistData);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  return {};
+};
+
+
+const Tab = ({id, label, children}: {
+  id: string;
+  label: string;
+  children: ReactNode | JSX.Element | JSX.Element[];
+}) => {
+  return null;//<>{children}</>;
+};
+
+const Tabs = ({selected, children, onChange}: {
+  onChange: (id: string) => void;
+  selected: string;
+  children: React.ReactElement<{
+    id: string;
+    label: string;
+    children: React.ReactElement;
+  }> | React.ReactElement<{
+    id: string;
+    label: string;
+    children: React.ReactElement;
+  }>[];
+}) => {
+  return (
+    <div className="tab-container">
+      <div className="tab-bar">
+        {React.Children.map(children, ({props: {id, label}}) => {
+          return (
+            <div key={id}
+                 className="tab-button"
+                 data-active={id === selected}
+                 onClick={() => onChange(id)}
+            >
+              {label}
+            </div>
+          )
+        })}
+      </div>
+      <div className="tab-content">
+        {React.Children.toArray(children).filter((tab: React.ReactNode) => {
+          return tab && (typeof tab === 'object') && ('props' in tab) && tab.props?.id === selected;
+        }).map(tab => (tab as JSX.Element).props.children)}
+      </div>
+    </div>
+  );
+};
+
+
+export const App = () => {
+  const [tab, setTab] = useState('0');
+  const [tasks, setTasks] = useState<ITodoListIndex>({});
+
+  const deleteTask = useCallback((task: TodoItem) => {
+    const newTasks = {...tasks};
+    delete newTasks[task.id];
+    setTasks(newTasks);
+    save(newTasks);
+  }, [tasks, setTasks]);
+
+  const addTask = useCallback((parent: string | undefined) => {
+    const task = createTask(parent);
+    const newTasks = {
+      ...tasks,
+      [task.id]: task,
+    };
+    setTasks(newTasks);
+    save(newTasks);
+  }, [setTasks, tasks]);
+
+  const updateTask = useCallback((task: TodoItem, values: TaskFields) => {
+    const newTasks = {
+      ...tasks,
+      [task.id]: {
+        ...task,
+        ...values,
+      },
+    };
+    setTasks(newTasks);
+    save(newTasks);
+  }, [tasks, setTasks]);
+
+  useEffect(() => {
+    setTasks(load());
+  }, []);
+
+  return (
+    <div className="App">
+      <Tabs selected={tab} onChange={setTab}>
+        <Tab id="0" label="Cards">
+          <h1>Forest of Tasks</h1>
+          <TodoList tasks={tasks}
+                    addTask={addTask}
+                    deleteTask={deleteTask}
+                    updateTask={updateTask}
+          />
+        </Tab>
+        <Tab id="1" label="Tree">
+          Tree
+        </Tab>
+        <Tab id="2" label="Menu 3">
+          Tree
+        </Tab>
+        <Tab id="3" label="Menu 4">
+          Tree
+        </Tab>
+      </Tabs>
     </div>
   );
 };
